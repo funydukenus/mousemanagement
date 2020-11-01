68e90f0f18ef717bfba841700f1ef6a1a4666d29
YuHangChen <chenyuhang01@gmail.com>
2020-11-01 17:24:47 +0800

    when delete all, use force to get the mouse in db

--- harvestmouseapp/mvc_model/mouseController.py
+++ harvestmouseapp/mvc_model/mouseController.py
@@ -67,6 +67,9 @@ class MouseController:
         """
         filtered_moust_list = self._db_adapter.get_all_mouse(force)
 
+        if filtered_moust_list.count() == 0:
+            filtered_moust_list = self._mouse_viewer.transform(filtered_moust_list=None)
+
         if transform:
             if filter_option is not None:
                 # Filtered mouse list can ba a list of mouse or single individual mouse object
